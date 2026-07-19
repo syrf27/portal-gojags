d5585729d9f6c418a8c76f291e14b36e43c92b78
syrf27 <asahclass@gmail.com>
2026-07-19 09:09:21 +0700

    feat: implement SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://portal-gojags-alpha.vercel.app',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
